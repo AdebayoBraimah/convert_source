8764dc25e7529dc08b8ba12e7cb007fc34dbe42d
AdebayoBraimah <adebayo.braimah@gmail.com>
2021-01-30 14:28:22 -0700

    Partially figuring out python package path issue

---
+++ tests/test2.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+# This works 
+
+import sys
+
+# print(__name__)
+# print(sys.path)
+
+pkg_path = "/Users/adebayobraimah/Desktop/projects/convert_source"
+
+sys.path.append(pkg_path)
+
+from convert_source import cs_utils
