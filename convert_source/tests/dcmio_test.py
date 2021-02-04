f13816c76765b854c115a0e9ccabef208639257a
AdebayoBraimah <adebayo.braimah@gmail.com>
2021-02-03 21:33:16 -0700

    Added test functions for data collection of DICOMs

--- convert_source/tests/dcmio_test.py
+++ convert_source/tests/dcmio_test.py
@@ -8,11 +8,13 @@ NOTE:
         * This function wraps `get_dcm_scan_tech` and `get_par_scan_tech`.
 """
 
+from convert_source.cs_utils.utils import SubDataInfo
 import pytest
 
 import os
-import pathlib
 import sys
+import pathlib
+import platform
 
 from typing import (
     Dict,
@@ -29,17 +31,20 @@ sys.path.append(mod_path)
 
 from convert_source.cs_utils.fileio import (
     Command,
-    DependencyError,
-    File
+    DependencyError
 )
 
-# from convert_source.cs_utils.utils import (
-#     # imports
-# )
-# 
+from convert_source.cs_utils.utils import (
+    SubDataInfo,
+    collect_info
+)
+
 # from convert_source.imgio.dcmio import (
 #     # imports
 # )
+
+# Maximally compress data:
+# GZIP=-9 tar -cvzf <file.tar.gz> <directory>
 
 # Windows commands
 # 
@@ -52,6 +57,78 @@ from convert_source.cs_utils.fileio import (
 # Uncompress *.tar.gz file: tar -zxvf <file.tar.gz>
 
 # Test variables
-scripts_dir: str = os.path.join(os.getcwd(),'TEST001-UNIT001','data.dicom')
+data_dir: str = os.path.join(os.getcwd(),'test.study_dir','TEST001-UNIT001')
+dcm_test_data: str = os.path.join(data_dir,'data.dicom','ST000000')
 tmp_out: str = os.path.join(os.getcwd(),'tmp.subs.dir')
+scripts_dir: str = os.path.join(os.getcwd(),'helper.scripts')
 tmp_json: str = os.path.join(scripts_dir,'test.orig.json')
+
+def test_extract_data():
+    dcm_data: str = os.path.join(data_dir,'data.dicom','data.tar.gz')
+    extract: Command = Command("tar")
+    extract.cmd_list.append("-zxvf")
+    extract.cmd_list.append(dcm_data)
+    extract.cmd_list.append("-C")
+    extract.cmd_list.append(
+        os.path.dirname(dcm_data)
+    )
+    extract.run()
+    assert os.path.exists(dcm_test_data) == True
+
+def get_subject_data():
+    subs_data: List[SubDataInfo] = collect_info(parent_dir=data_dir,
+                                                exclusion_list=[".PAR", ".nii"])
+    assert len(subs_data) == 3
+
+# Intermediary test variables
+subs_data: List[SubDataInfo] = collect_info(parent_dir=data_dir,
+                                            exclusion_list=[".PAR", ".nii"])
+
+sub: str = subs_data[0].sub
+ses: str = subs_data[0].ses
+data: str = subs_data[0].data
+
+def test_get_scan_time():
+
+# def test_cleanup():
+#     del_method: bool = False
+#     rm_method: bool = False
+#     rm_item_method: bool = False
+
+#     if platform.system().lower() != 'windows':
+#         rm_test_dir: Command = Command("rm")
+#         rm_test_dir.cmd_list.append("-rf")
+#         rm_test_dir.cmd_list.append(dcm_test_data)
+#         rm_test_dir.run()
+#         assert os.path.exists(dcm_test_data) == False
+#     else:
+#         try:
+#             rm_test_dir: Command = Command("del")
+#             rm_test_dir.check_dependency()
+#             del_method: bool = True
+#         except DependencyError:
+#             rm_test_dir: Command = Command("rmdir")
+#             rm_test_dir.check_dependency()
+#             rm_method: bool = True
+#         except DependencyError:
+#             rm_test_dir: Command = Command("Remove-Item")
+#             rm_test_dir.check_dependency()
+#             rm_item_method: bool = True
+#         except DependencyError:
+#             import shutil
+#             shutil.rmtree(dcm_test_data)
+#             assert os.path.exists(dcm_test_data) == False
+        
+#         if rm_method or del_method:
+#             rm_test_dir.cmd_list.append("/s")
+#             rm_test_dir.cmd_list.append("/q")
+#             rm_test_dir.cmd_list.append("/f")
+#             rm_test_dir.cmd_list.append(dcm_test_data)
+#             rm_test_dir.run()
+#             assert os.path.exists(dcm_test_data) == False
+#         elif rm_item_method:
+#             rm_test_dir.cmd_list.append(dcm_test_data)
+#             rm_test_dir.cmd_list.append("-Recurse")
+#             rm_test_dir.run()
+#             assert os.path.exists(dcm_test_data) == False
+
