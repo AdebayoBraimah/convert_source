a3adc02c43d3b9dc9e67dcc5b4d8eac8ff1dd227
AdebayoBraimah <adebayo.braimah@gmail.com>
2021-02-12 20:03:56 -0700

    Began constructing executable file parser

--- bin/study_proc.py
+++ bin/study_proc.py
@@ -13,20 +13,17 @@ import argparse
 _pkg_path: str = str(pathlib.Path(os.path.abspath(__file__)).parents[1])
 sys.path.append(_pkg_path)
 
-# from convert_source import __version__
-# print(__version__)
+# from convert_source import __version__ as version
+# print(version)
 
 from convert_source.batch_convert import batch_proc
 
-# def basic_args():
-#     '''
-#     basic args
-#     '''
+def parser():
+    '''
+    working doc-string
+    '''
+    
 
-# def full_args():
-#     '''
-#     full args
-#     '''
 
 
 
