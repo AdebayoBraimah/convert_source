899566f37d71a578a11358b4d88a776e1270750b
AdebayoBraimah <adebayo.braimah@gmail.com>
2021-02-15 15:21:19 -0700

    Updated docs to use read the docs

---
+++ docs/build-docs.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+cwd=$(pwd)
+
+wd=$(dirname ${0})
+wd=$(pwd)
+
+# echo ${wd}
+# pip install sphinx sphinx-autodoc-typehints sphinx_rtd_theme sphinx-autodoc-napoleon-typehints sphinx-argparse
+
+# sphinx-quickstart
+# sphinx-apidoc -o source ../convert_source
+
+# make clean
+# make html
+
+sphinx-apidoc -o source ../../convert_source; make clean; make html
+
+cd ${cwd}
+
+# copy source directory to docs, update conf.py
+# 
+# cp -r build/html/* ../../docs
+# touch ../../docs/.nojekyll
+# rm -rf build
