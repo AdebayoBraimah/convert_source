0391164b28ffdef4cb8fae24bcbc308ae9fbc6ca
AdebayoBraimah <adebayo.braimah@gmail.com>
2021-05-23 12:26:10 -0700

    [skip ci] Added doc-strings to all functions

--- convert_source/cs_utils/database.py
+++ convert_source/cs_utils/database.py
@@ -45,6 +45,18 @@ def create_db(database: str,
             tables: OrderedDict
             ) -> str:
     """Creates database provided an ordered dictionary of table names and types.
+
+    Usage example:
+        >>> db = create_db(database='file.db',
+        ...                 tables=db_tables)
+        ...
+
+    Arguments:
+        database: Input database filename.
+        tables: Ordered dictionary, in which the 0th key is the primary key, and the items are the data type.
+
+    Returns:
+        String that corresponds to the database filename.
     """
     # Create/access database
     conn = sqlite3.connect(database)
@@ -69,7 +81,6 @@ def create_db(database: str,
             query: str = f"CREATE TABLE {tn} ({nf1} {ft1} PRIMARY KEY, {nf2} {ft2})"
             c.execute(query)
         except OperationalError:
-            # print(table_name)
             continue
     
     # Commit changes and close the connection
@@ -80,7 +91,22 @@ def create_db(database: str,
 def insert_row_db(database: str,
                 tables: OrderedDict,
                 info: Dict[str,str]) -> str:
-    """Inserts rows into existing database tables, provided a dictionary of key mapped items of values. 
+    """Inserts rows into existing database tables, provided a dictionary of key mapped items of values.
+
+    Usage example:
+        Usage example:
+        >>> db = insert_row_db(database='file.db',
+        ...                     tables=db_tables,
+        ...                     info=table_vals)
+        ...
+
+    Arguments:
+        database: Input database filename.
+        tables: Ordered dictionary, in which the 0th key is the primary key, and the items are the data type.
+        info: Dictionary with keys that correspond to tables, that contain coresponding values.
+
+    Returns:
+        String that corresponds to the database filename.
     """
     # Access database
     conn = sqlite3.connect(database)
@@ -112,7 +138,21 @@ def insert_row_db(database: str,
 def get_len_rows(database: str, 
                 tables: OrderedDict
                 ) -> int:
-    """Gets number of rows in a databases' table.
+    """Gets the number of rows in a databases' first table.
+
+    Usage example:
+        Usage example:
+        >>> get_len_rows(database='file.db',
+        ...              tables=db_tables)
+        ...
+        45
+
+    Arguments:
+        database: Input database filename.
+        tables: Ordered dictionary, in which the 0th key is the primary key, and the items are the data type.
+
+    Returns:
+        Integer that corresponds to the number of rows in the databases' first table.
     """
     # Access database
     conn = sqlite3.connect(database)
@@ -133,6 +173,22 @@ def get_file_id(database: str,
                 num_zeros: int = 7
                 ) -> str:
     """Returns new file_id for file that does not yet exist in the database.
+
+    Usage example:
+        Usage example:
+        >>> get_file_id(database='file.db',
+        ...             tables=db_tables,
+        ...             num_zeros=7)
+        ...
+        '0000045'
+
+    Arguments:
+        database: Input database filename.
+        tables: Ordered dictionary, in which the 0th key is the primary key, and the items are the data type.
+        num_zeros: Number of zeros to use to zeropad the output number.
+
+    Returns:
+        Zeropadded string for some unique file_id.
     """
     file_id: int = get_len_rows(database, tables) + 1
     file_id: str = zeropad(num=file_id, num_zeros=num_zeros)
@@ -141,14 +197,37 @@ def get_file_id(database: str,
 def update_table_row(database: str,
                     prim_key: str,
                     table_name: str, 
-                    col_name: str, 
+                    col_name: Optional[str], 
                     value: Optional[Union[int,str]]
                     ) -> str:
-    """Updates a row in a table in some given database.
+    """Updates a row in a table in some given database provided a table name and a value.
+
+    Usage example:
+        >>> db = update_table_row(database='file.db',
+        ...                       prim_key='file_id',
+        ...                       table_name='bids_name',
+        ...                       col_name='bids_name',
+        ...                       value='sub-001_ses-001_run-01_T1w')
+        ...
+
+    Arguments:
+        database: Input database filename.
+        prim_key: Required primary key used to index all rows in the database.
+        table_name: Table name of table in database to be updated.
+        col_name: Column name of the table to be updated. If not provided, then the column name is assumed to be table name.
+        value: The value (integer or string) used to update the column in the table/database.
+
+    Returns:
+        String that corresponds to the database filename.
     """
     # Access database
     conn = sqlite3.connect(database)
     c = conn.cursor()
+
+    if col_name:
+        pass
+    else:
+        col_name: str = table_name
 
     # Perform database table update
     query: str = f"UPDATE {table_name} SET {col_name} = ? WHERE {list(tables.keys())[0]} = ?"
@@ -163,6 +242,18 @@ def export_dataframe(database: str,
                     tables: OrderedDict
                     ) -> pd.DataFrame:
     """Exports all of the tables from the input database as a dataframe.
+
+    Usage example:
+        >>> df = export_dataframe(database='file.db',
+        ...                       tables=db_tables)
+        ...
+
+    Arguments:
+        database: Input database filename.
+        tables: Ordered dictionary, in which the 0th key is the primary key, and the items are the data type.
+
+    Returns:
+        Dataframe of all of the tables in the database.
     """
     # Access database
     conn = sqlite3.connect(database)
@@ -187,6 +278,25 @@ def export_scans_dataframe(database: str,
                             *args: str
                             ) -> pd.DataFrame:
     """Exports a dataframe provided table/column IDs.
+
+    Usage example:
+        >>> df = export_scans_dataframe(database='file.db',
+        ...                             raise_exec=False,
+        ...                             'sub_id',
+        ...                             'ses_id',
+        ...                             'bids_name')
+        ...
+
+    Arguments:
+        database: Input database filename.
+        raise_exec: Boolean to raise exception.
+        *args: Name arguments that correspond to table names of the input database.
+
+    Returns:
+        Dataframe of select tables from the database
+
+    Raises:
+        DatabaseError: Error that arises should the table not be in the database and 'raise_exec' is True.
     """
     # Access database
     conn = sqlite3.connect(database)
@@ -203,9 +313,9 @@ def export_scans_dataframe(database: str,
 
         if c.fetchone()[0] == 1:
             pass
+        elif raise_exec:
+            raise DatabaseError(f"Table {table} does not exist in database")
         else:
-            if raise_exec:
-                raise DatabaseError(f"Table {table} does not exist in database")
             continue
         
         # BUG: This SQL query should include the sub_id val in place of '*' for subject exclusivity
@@ -215,10 +325,23 @@ def export_scans_dataframe(database: str,
 
     return pd.concat(df_list,axis=1,join='outer')
 
-def get_dir_relative_path(study_dir: str,
+def _get_dir_relative_path(study_dir: str,
                         file_name: str
                         ) -> str:
-    """Returns the relative path provided some parent study directory and some file name.
+    """Helper function that returns the relative path provided some parent study directory and some file name - for record keeping purposes.
+
+    Usage example:
+        >>> _get_dir_relative_path(study_dir='/<parent>/<dir>',
+        ...                        file_name='/<parent>/<dir>/<sub_data>/<image_data>/file0001.dcm')
+        ...
+        './<dir>/<sub_data>/<image_data>/file0001.dcm'
+
+    Arguments:
+        study_dir: Parent study directory that contains all of the subjects' imaging data.
+        file_name: Filename/path of image data.
+
+    Returns:
+        String that corresponds to the relative path of the imaging data.
     """
     path_sep: str = os.path.sep
     dir_tmp = str(pathlib.Path(study_dir).parents[0])
