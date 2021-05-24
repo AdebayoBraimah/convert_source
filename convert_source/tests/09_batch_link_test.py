d6ca17545c82a35a99f73897eed975f508f5beb2
AdebayoBraimah <adebayo.braimah@gmail.com>
2021-05-24 03:32:45 -0700

    [skip ci] Wrote initial lines for test script

--- convert_source/tests/09_batch_link_test.py
+++ convert_source/tests/09_batch_link_test.py
@@ -1,5 +1,27 @@
 # -*- coding: utf-8 -*-
-"""doc-string
+"""Tests for convert_source's batch symbolic linking functions.
 """
-# TODO:
-#   Write unit tests
+import pytest
+
+import os
+import sys
+import pathlib
+import platform
+import urllib.request
+import shutil
+
+from copy import deepcopy
+from typing import (
+    Dict,
+    List
+)
+
+# Add package/module to PYTHONPATH
+mod_path: str = os.path.join(str(pathlib.Path(os.path.abspath(__file__)).parents[2]))
+sys.path.append(mod_path)
+
+from convert_source.batch_symlink import (
+    batch_link,
+    read_file_to_list,
+    create_study_sym_links
+)
